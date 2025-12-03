5324e8bc856f05162f0f9bf66605a27bdf328fd4
Eliane Moreira <160543637+ElianeMoreira@users.noreply.github.com>
2025-12-03 20:14:27 +0000

    Gerando mais estatisticas

--- lib/estatisticas/comparacoes.py
+++ lib/estatisticas/comparacoes.py
@@ -1,9 +1,10 @@
 from typing import Dict, Callable, List
 import time 
-from .texto import indice_coincidencia
+from .texto import indice_coincidencia, entropia, gerar_dados_cripto_graficos
 from .algoritmos import medir_tempo, expansao_tamanho, calcular_avalanche
 
-def comparar_algoritmos(textos: Dict[str, str], algoritmos: Dict[str, Dict[str, Callable]]) -> Dict[str, Dict[str, Dict[str, float]]]:
+
+def comparar_algoritmos(textos: Dict[str, str], algoritmos: Dict[str, Dict[str, Callable]], max_shift_auto: int = 50):
     """
     Compara tempo de cifragem/decifragem e métricas de criptoanálise para vários algoritmos e textos.
     """
@@ -11,27 +12,34 @@ def comparar_algoritmos(textos: Dict[str, str], algoritmos: Dict[str, Dict[str, 
 
     for nome_alg, funcs in algoritmos.items():
         resultados[nome_alg] = {}
-        func_cifrar = funcs["cifrar"]
-        func_decifrar = funcs["decifrar"]
-        func_ataque = funcs["atacar"]
-        
+
         for nome_txt, texto in textos.items():
-            
-            start_time_cifra = time.perf_counter()
-            cifrado = func_cifrar(texto) 
-            tempo_cifra = time.perf_counter() - start_time_cifra
-            tempo_decifra = medir_tempo(func_decifrar, cifrado)
-            tempo_ataque = medir_tempo(func_ataque, cifrado)
+
+            # tempo
+            start = time.perf_counter()
+            cifrado = funcs["cifrar"](texto)
+            tempo_cifra = time.perf_counter() - start
+
+            tempo_decifra = medir_tempo(funcs["decifrar"], cifrado)
+            tempo_ataque = medir_tempo(funcs["atacar"], cifrado)
+
             tamanho_bytes = len(texto.encode('utf-8'))
-            
+
+            # numeros matriz
+            dados_graf = gerar_dados_cripto_graficos(texto, funcs["cifrar"], max_shift_auto)
+
             resultados[nome_alg][nome_txt] = {
                 "tamanho": tamanho_bytes,
                 "tempo_cifra": tempo_cifra,
                 "tempo_decifra": tempo_decifra,
                 "tempo_ataque": tempo_ataque,
+
                 "IC": indice_coincidencia(cifrado),
                 "expansao": expansao_tamanho(len(texto), len(cifrado)),
-                "avalanche": calcular_avalanche(func_cifrar, texto),
+                "avalanche": calcular_avalanche(funcs["cifrar"], texto),
+                "entropia": entropia(cifrado),
+                "autocorrelacao": dados_graf["autocorrelacao"],
+                "coocorrencia": dados_graf["coocorrencia"],
             }
 
     return resultados
