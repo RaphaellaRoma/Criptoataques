8bf547f6127ee5d3506fb6f9aa7b98efef8cba6c
Raphaella Roma <raphysics.oficial@gmail.com>
2025-12-03 13:58:53 -0300

    alterações

---
+++ lib/ataques/cifra_de_vigenere/ataque.py
@@ -0,0 +1,8 @@
+from lib.ataques.cifra_de_vigenere.vigenere import VigenereCifra
+
+
+def atacar(texto_cifrado):
+    vigenere = VigenereCifra()
+    tamanho_chave = vigenere.tamanho_chave(texto_cifrado)
+    chave = vigenere.quebra_chave(texto_cifrado, tamanho_chave)
+    return vigenere.encriptar_decriptar(texto_cifrado,chave,"decifrar")
